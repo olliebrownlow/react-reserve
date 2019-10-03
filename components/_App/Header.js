b77139528a5067aa8fe917434ffdf302e99ca32e
olliebrownlow <olliebrownlow@gmail.com>
2019-10-03 16:59:18 +0100

    gets useRouter hook for route data and implements active links

--- components/_App/Header.js
+++ components/_App/Header.js
@@ -1,14 +1,20 @@
 import { Menu, Container, Image, Icon } from 'semantic-ui-react';
 import Link from 'next/link';
+import { useRouter } from 'next/router'
 
 function Header() {
+  const router = useRouter();
   const user = false;
+
+  function isActive(route) {
+    return route === router.pathname;
+  }
 
   return (
     <Menu fluid id='menu' inverted>
       <Container text>
         <Link href="/">
-          <Menu.Item header>
+          <Menu.Item header active={isActive('/')}>
             <Image
               size="mini"
               src="/static/logo.svg"   
@@ -19,7 +25,7 @@ function Header() {
         </Link>
 
         <Link href="/cart">
-          <Menu.Item header>
+          <Menu.Item header active={isActive('/cart')}>
             <Icon
               name="cart"
               size="large"
@@ -29,7 +35,7 @@ function Header() {
         </Link>
 
         { user && <Link href="/create">
-          <Menu.Item header>
+          <Menu.Item header active={isActive('/create')}>
             <Icon
               name="add square"
               size="large"
@@ -41,7 +47,7 @@ function Header() {
         {user ? (
           <>
           <Link href="/account">
-            <Menu.Item header>
+            <Menu.Item header active={isActive('/account')}>
               <Icon
                 name="user"
                 size="large"
@@ -61,7 +67,7 @@ function Header() {
         ) : (
           <>
           <Link href="/login">
-            <Menu.Item header>
+            <Menu.Item header active={isActive('/login')}>
               <Icon
                 name="sign in"
                 size="large"
@@ -71,7 +77,7 @@ function Header() {
           </Link>
 
           <Link href="/signup">
-            <Menu.Item header>
+            <Menu.Item header active={isActive('/signup')}>
               <Icon
                 name="signup"
                 size="large"
