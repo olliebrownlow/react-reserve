aacbedeafbaa949d6d65888704985ba14582283f
olliebrownlow <olliebrownlow@gmail.com>
2019-10-07 13:56:25 +0100

    exported initial products to the MongoDB database, creates product schema for each product with the mongoose package, fetches data from the database to use in the client

--- pages/api/products.js
+++ pages/api/products.js
@@ -1,9 +1,11 @@
-import products from '../../static/products.json';
+// import products from '../../static/products.json';
+import Product from '../../models/Product';
 import connectDb from '../../utils/connectDb';
 
 connectDb();
 
-export default (req, res) => {
+export default async (req, res) => {
   console.log(req.method);
+  const products = await Product.find();
   res.status(200).json(products)
 };
