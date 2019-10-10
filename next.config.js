154098f5a63349e9d1113c8d2def7b98a6e202b0
olliebrownlow <olliebrownlow@gmail.com>
2019-10-10 23:40:16 +0100

    Uploads new product image to cloudinary and posts new products to database

--- next.config.js
+++ next.config.js
@@ -4,7 +4,7 @@ module.exports = {
     MONGO_SRV:
       "mongodb+srv://OllieBrownlow:columbus@reactreserve-e0gc4.mongodb.net/test?retryWrites=true&w=majority",
     JWT_SECRET: "<insert-jwt-secret>",
-    CLOUDINARY_URL: "<insert-cloudinary-url>",
+    CLOUDINARY_URL: "https://api.cloudinary.com/v1_1/olliescloud/image/upload",
     STRIPE_SECRET_KEY: "<insert-stripe-secret-key>"
   }
 };
