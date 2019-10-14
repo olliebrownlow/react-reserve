e3861ef1141582459f4b0948e37a3d7b18bf1532
olliebrownlow <olliebrownlow@gmail.com>
2019-10-14 23:07:54 +0100

    creates User model, signs up user with axios POST request, jsonwebtoken and cookie, saves new User to database, redirects to account page

--- next.config.js
+++ next.config.js
@@ -3,7 +3,7 @@ module.exports = {
   env: {
     MONGO_SRV:
       "mongodb+srv://OllieBrownlow:columbus@reactreserve-e0gc4.mongodb.net/test?retryWrites=true&w=majority",
-    JWT_SECRET: "<insert-jwt-secret>",
+    JWT_SECRET: "powknwdcihgfwoiqmcejuo",
     CLOUDINARY_URL: "https://api.cloudinary.com/v1_1/olliescloud/image/upload",
     STRIPE_SECRET_KEY: "<insert-stripe-secret-key>"
   }
