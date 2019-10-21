772d82b21203644e27f3cafc9310ac3e8f785c8a
olliebrownlow <olliebrownlow@gmail.com>
2019-10-22 00:14:49 +0100

    creates Order model, creates checkout endpoint, successfully checksout customer carts

--- next.config.js
+++ next.config.js
@@ -5,6 +5,6 @@ module.exports = {
       "mongodb+srv://OllieBrownlow:columbus@reactreserve-e0gc4.mongodb.net/test?retryWrites=true&w=majority",
     JWT_SECRET: "powknwdcihgfwoiqmcejuo",
     CLOUDINARY_URL: "https://api.cloudinary.com/v1_1/olliescloud/image/upload",
-    STRIPE_SECRET_KEY: "<insert-stripe-secret-key>"
+    STRIPE_SECRET_KEY: "sk_test_SI5VmKt0K5zr44zSvIa4RsDN008lrDkw0v"
   }
 };
