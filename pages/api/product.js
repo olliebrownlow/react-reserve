20453adbba91d11c1153644308fb2d43cbe03bce
olliebrownlow <olliebrownlow@gmail.com>
2019-10-23 21:53:56 +0100

    deletes products from carts when product is deleted from db

--- pages/api/product.js
+++ pages/api/product.js
@@ -1,4 +1,5 @@
 import Product from "../../models/Product";
+import Cart from "../../models/Cart";
 import connectDb from "../../utils/connectDb";
 
 connectDb();
@@ -48,6 +49,17 @@ async function handlePostRequest(req, res) {
 
 async function handleDeleteRequest(req, res) {
   const { _id } = req.query;
-  await Product.findOneAndDelete({ _id });
-  res.status(204).json({});
+  try {
+    // Delete product by Id
+    await Product.findOneAndDelete({ _id });
+    // Remove product from all carts, referenced as 'product'
+    await Cart.updateMany(
+      { "products.product": _id },
+      { $pull: { products: { product: _id } } }
+    );
+    res.status(204).json({});
+  } catch (error) {
+    console.error(error);
+    res.status(500).send("Error deleting product");
+  }
 }
